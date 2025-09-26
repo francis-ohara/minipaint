479c3f8425979691cd279b0678798f2d24fc7b0c
francis-ohara <franciskohara@gmail.com>
2025-09-26 00:56:52 -0400

    docs: add package documentation

---
+++ src/minipaint/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * The root package for the MiniPaint application.
+ * <p>
+ * This package contains the main application class ({@link minipaint.MiniPaint}), the drawing canvas ({@link minipaint.CanvasPanel}) and a tools subpackage containing classes for all the Tools in the MiniPaint applciation.
+ */
+package minipaint;
